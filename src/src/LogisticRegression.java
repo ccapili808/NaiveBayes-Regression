f0beaeed0e3af65d868bf86b821422304a2d75ea
Vasileios Grigorios Kourakos <grigorisk@unm.edu>
2023-03-15 20:42:00 -0600

    Switched to float matrices, fixed some of the matrix operations and scaling/normalization.

--- src/src/LogisticRegression.java
+++ src/src/LogisticRegression.java
@@ -1,4 +1,7 @@
+import org.ojalgo.data.DataProcessors;
+import org.ojalgo.function.PrimitiveFunction;
 import org.ojalgo.matrix.store.ElementsSupplier;
+import org.ojalgo.matrix.store.Primitive32Store;
 import org.ojalgo.matrix.store.Primitive64Store;
 import org.ojalgo.matrix.store.SparseStore;
 
@@ -10,44 +13,38 @@ import static org.ojalgo.function.constant.PrimitiveMath.*;
 
 
 public class LogisticRegression {
-
     private String vocabularyFile = "vocabulary.txt";
     private String trainingFile = "training.csv";
     private String classificationFile = "newsgrouplabels.txt";
     private String testingFile = "testing.csv";
 
-    private Primitive64Store xMatrix = Primitive64Store.FACTORY.make(12000,61189);
-    private Primitive64Store classificationsMatrix = Primitive64Store.FACTORY.make(12000, 1);
-    private Primitive64Store probabilities = Primitive64Store.FACTORY.make(20,12000);
-    private Primitive64Store weightsMatrix = Primitive64Store.FACTORY.make(20,61189);
-    private Primitive64Store deltaMatrix = Primitive64Store.FACTORY.make(20,12000);
-    private SparseStore<Double> xTranspose;
+    private Primitive32Store xMatrix = Primitive32Store.FACTORY.make(12000,61189);
+    private Primitive32Store classificationsMatrix = Primitive32Store.FACTORY.make(12000, 1);
+    private Primitive32Store probabilities = Primitive32Store.FACTORY.make(20,12000);
+    private Primitive32Store weightsMatrix = Primitive32Store.FACTORY.make(20,61189);
+    private Primitive32Store deltaMatrix = Primitive32Store.FACTORY.make(20,12000);
+    private Primitive32Store xTranspose = Primitive32Store.FACTORY.make(61189,12000);
+    PrimitiveFunction.Unary matrixOperation = arg -> {
+        if (Math.exp(arg) > 20 || Float.isInfinite((float)Math.exp(arg))) {
+            return 20;
+        }
+        else return Math.exp(arg);
+    };
 
 
     //the learning rate
-    private double eta;
+    private float eta;
 
     //the penalty term
-    private double lambda;
+    private float lambda;
 
     //the number of iterations
     private final int ITERATIONS = 10;
 
 
-    public LogisticRegression(double lambda){
-        this.eta = 0.001;
+    public LogisticRegression(float lambda){
+        this.eta = 0.001f;
         this.lambda = lambda;
-        try {
-            createDataSet();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        train();
-    }
-
-    public LogisticRegression() {
-        eta = 0.001;
-        lambda = 0.01;
         try {
             createDataSet();
         } catch (FileNotFoundException e) {
@@ -61,16 +58,17 @@ public class LogisticRegression {
         //initialize random weights
         for(int i =0; i <20;i++) {
             for(int j = 0; j<61189;j++) {
-               weightsMatrix.set(i,j,(0+(0.1*r.nextDouble())));
+                weightsMatrix.set(i,j,((0.001f*r.nextFloat())));
             }
         }
         System.out.println("Reading training set...");
         Scanner sc = null;
         sc = new Scanner(new File(trainingFile));
         //read every line in training set and build the data needed
-        while (sc.hasNextLine()) {
+        //while (sc.hasNextLine()) {
+        for(int l = 0; l<12000;l++) {
             String[] line = sc.nextLine().split(",");
-            int classification = Integer.parseInt(line[line.length-1]);
+            float classification = Float.parseFloat(line[line.length-1]);
             int documentID = Integer.parseInt(line[0]);
             //print out the document number to see where the code is at
             if(documentID%100 == 0) {
@@ -79,15 +77,18 @@ public class LogisticRegression {
             for (int j = 0; j<20; j++) {
                 //set the delta matrix
                 if (classification == j) {
-                    deltaMatrix.set(j,documentID-1,1);
+                    deltaMatrix.set(j,documentID-1,1f);
+                }
+                else {
+                    deltaMatrix.set(j,documentID-1,0.0f);
                 }
             }
             //add the document's classification to the matrix
             classificationsMatrix.set(documentID-1,0,classification);
             //set x0 to 1
-            xMatrix.set(documentID-1,0,1);
+            xMatrix.set(documentID-1,0,1f);
             for (int i=1; i < (line.length-2); i++) {
-                double wordCount = Double.parseDouble(line[i]);
+                float wordCount = Float.parseFloat(line[i]);
                 //add specific word count from each document to the total count of that word to the example matrix
                 if(wordCount>0) {
                     xMatrix.set(documentID-1,i,wordCount);
@@ -95,7 +96,9 @@ public class LogisticRegression {
             }
         }
         //get the transpose of the X matrix and make it into a SparseStore for efficient multiplication
-        xTranspose = SparseStore.PRIMITIVE64.make(xMatrix.transpose());
+        ElementsSupplier temp1 = xMatrix.transpose();
+        temp1.supplyTo(xTranspose);
+
     }
 
     public void train(){
@@ -107,13 +110,12 @@ public class LogisticRegression {
 
             //perform matrix operations using ojAlgo java library
 
-            ElementsSupplier<Double> temp1;
-            ElementsSupplier<Double> temp2;
-            ElementsSupplier<Double> temp3;
-            ElementsSupplier<Double> temp4;
-            ElementsSupplier<Double> temp5;
-            Primitive64Store matrix1 = Primitive64Store.FACTORY.make(20,12000);
-            Primitive64Store matrix2 = Primitive64Store.FACTORY.make(20,61189);
+            ElementsSupplier temp1;
+            ElementsSupplier temp2;
+            ElementsSupplier temp3;
+            ElementsSupplier temp4;
+            Primitive32Store matrix1 = Primitive32Store.FACTORY.make(20,12000);
+            Primitive32Store matrix2 = Primitive32Store.FACTORY.make(20,61189);
 
             temp1 = deltaMatrix.onMatching(SUBTRACT, probabilities);
             temp1.supplyTo(matrix1);
@@ -121,31 +123,61 @@ public class LogisticRegression {
             temp3 = weightsMatrix.onAll(MULTIPLY, lambda);
             temp3.supplyTo(matrix2);
             temp4 = temp2.onMatching(SUBTRACT,matrix2);
-            temp5 = temp4.onAll(MULTIPLY, eta);
-            temp5.onMatching(ADD, weightsMatrix);
-            temp5.supplyTo(weightsMatrix);
+            temp4.onAll(MULTIPLY, eta);
+            temp4.onMatching(ADD, weightsMatrix);
+            temp4.supplyTo(weightsMatrix);
+            weightsMatrix.modifyAny(DataProcessors.CENTER_AND_SCALE);
+
         }
     }
 
     public void calculateProbabilities() {
+
         ElementsSupplier temp;
         //multiply weights by x transpose
         temp = weightsMatrix.multiply(xTranspose);
         //make every element e^i
-        temp.onAll(EXP);
-        //put into probabilities matrix
+
         temp.supplyTo(probabilities);
+        for(int i = 0; i < 12000; i++) {
+            probabilities.set(19,i,0);
+        }
+        probabilities.modifyAny(DataProcessors.SCALE);
+        probabilities.onAll(matrixOperation);
+
         //normalize probability columns
         for(int i = 0;i < 12000; i++) {
-            probabilities.set(19,i,1);
-            double total = 0;
+            probabilities.set(19,i,1f);
+            float total = 0.0f;
             for (int j = 0; j < 20; j++) {
                 total += probabilities.get(j,i);
             }
-            for (int j = 0; j < 20; j++) {
-                probabilities.set(j,i, (probabilities.get(j,i)/total));
+            if (total != 0.0f) {
+                for (int j = 0; j < 20; j++) {
+                    probabilities.set(j, i, (float) (probabilities.get(j, i) / total));
+                }
             }
         }
+    }
+
+    public void setVocabularyFile(String vocabularyFile) {
+        this.vocabularyFile = vocabularyFile;
+    }
+
+    public void setTrainingFile(String trainingFile) {
+        this.trainingFile = trainingFile;
+    }
+
+    public void setClassificationFile(String classificationFile) {
+        this.classificationFile = classificationFile;
+    }
+
+    public void setTestingFile(String testingFile) {
+        this.testingFile = testingFile;
+    }
+
+    public void setLambda(float lambda) {
+        this.lambda = lambda;
     }
 
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -4,13 +4,58 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args){
-        try {
-            NaiveBayes naiveBayes = new NaiveBayes("default");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        while(prompt() == 1) {
+            prompt();
         }
+    }
+
+    /**
+     * Prompts the user to choose which algorithm to run.
+     * If the user chooses Naive Bayes, they are prompted to enter a beta value.
+     * If the user chooses Logistic Regression, they are prompted to enter a lambda value.
+     */
+    public static int prompt() {
+        //Run in a loop until the user chooses to exit
+        System.out.println("This program runs Naive Bayes and Logistic Regression on the Newsgroups dataset.");
+        System.out.println("Please enter the number of the algorithm you would like to run:");
+        System.out.println("[1] Naive Bayes");
+        System.out.println("[2] Logistic Regression");
+        System.out.println("[3] Exit");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> {
+                //Naive Bayes
+                String beta;
+                System.out.println("Please enter the beta value you would like to use:");
+                beta = scanner.next();
+                try {
+                    NaiveBayes naiveBayes = new NaiveBayes(beta);
+                } catch (FileNotFoundException | UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            }
+            case 2 -> {
+                //Logistic Regression
+                System.out.println("Please enter the lambda value you would like to use:");
+                float lambda = scanner.nextFloat();
+                LogisticRegression logisticRegression = new LogisticRegression(lambda);
+                break;
+            }
+            case 3 -> {
+                //Exit
+                System.out.println("Exiting...");
+                return 0;
+            }
+            default -> {
+                System.out.println("Invalid choice. Please try again.");
+                prompt();
+                break;
+            }
+        }
+        scanner.close();
+        return 1;
     }
 
 }
